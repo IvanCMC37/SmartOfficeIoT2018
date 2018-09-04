71a6cbba77ce7d91556ea8c239e49576b42e48ba
rmit-s3665001-david-brenecki <s3665001@student.rmit.edu.au>
2018-09-04 03:10:54 +0000

    created pylint file

--- app.py
+++ app.py
@@ -4,9 +4,23 @@ APP = Flask(__name__)
 
 
 @APP.route("/")
-def hello():
-    """Load the homepage"""
+def homepage():
     return "<h1>Hello World</h1>"
+
+
+@APP.route("/doctors")
+def doctors_page():
+    return "<h2>doctors page</h2>"
+
+
+@APP.route("/patients")
+def patients_page():
+    return "<h2>patients page"
+
+
+@APP.route("/clerks")
+def clerks_page():
+    return "<h2>clerks page"
 
 
 if __name__ == "__main__":
